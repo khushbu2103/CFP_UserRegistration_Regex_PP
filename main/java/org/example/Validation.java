67843813ba18472d5dc822fd3325e6c65ef870c4
khushbu <khushbusoni475@gmail.com>
2024-01-04 12:09:18 +0530

    [khushbu] adding user registration validation by lambda expression

--- main/java/org/example/Validation.java
+++ main/java/org/example/Validation.java
@@ -1,154 +1,117 @@
 package org.example;
 
+import java.util.function.Function;
 import java.util.regex.Pattern;
 
 public class Validation {
     public static boolean firstNameValidation() throws CustomException {
         String firstName = "Khushi";
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(firstName).matches()) {
-            System.out.println(firstName + ": Valid first name!");
-        } else {
-            System.out.println(firstName + ": Invalid first name!");
-            throw new CustomException("Invalid first name");
+        Function<String, Boolean> validateFirstName = name -> name.matches("^[A-Z]{1}[a-z]{2,}$");
+        if (validateFirstName.apply(firstName))
+        {
+            System.out.println("First Name is valid: " + validateFirstName.apply(firstName));
+            return true;
         }
-        return Pattern.matches(regex, firstName);
+
+        throw new CustomException("Invalid first name");
     }
     public static boolean lastNameValidation() throws CustomException {
         String lastName = "Soni";
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        if(pattern.matcher(lastName).matches())
+        Function<String, Boolean> validateFirstName = name -> name.matches("^[A-Z]{1}[a-z]{2,}$");
+        if (validateFirstName.apply(lastName))
         {
-            System.out.println(lastName + ": Valid last name!");
+            System.out.println("Last Name is valid: " + validateFirstName.apply(lastName));
+            return true;
         }
-        else
-        {
-            System.out.println(lastName + ": Invalid last name!");
-            throw new CustomException("Invalid last name");
-        }
-        return Pattern.matches(regex, lastName);
+        throw new CustomException("Invalid last name");
     }
     public static boolean emailValidation() throws CustomException {
         String email = "abc.xyz@bl.co.in";
-        // String[] inputs = {"abc.xyz@bl.co.in", "yhz.out&kl.co.in", "abc.ykj@bl.co.ik"};
-        String regex = "^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(email).matches()) {
-            System.out.println( email + ": Valid email!");
-        }
-        else
+        Function<String, Boolean> validateEmail = name -> name.matches("^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$");
+        if (validateEmail.apply(email))
         {
-            System.out.println(email + ": Invalid email.");
-            throw new CustomException("Invalid email");
+            System.out.println("Email is valid: " + validateEmail.apply(email));
+            return true;
         }
-        return Pattern.matches(regex, email);
+        throw new CustomException("Invalid email");
     }
     public static boolean preDefinedMobileNumber() throws CustomException {
         String mobile = "91 9460984883";
-        //String[] inputs = {"91 9460984883", "946098488398", "912 7654327654"};
-        String regex = "^[0-9]{2}[ ][0-9]{10}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(mobile).matches()) {
-            System.out.println( mobile + ": Valid mobile number!");
-        }
-        else
+        Function<String, Boolean> validateMobileNumber = name -> name.matches("^[0-9]{2}[ ][0-9]{10}$");
+        if (validateMobileNumber.apply(mobile))
         {
-            System.out.println(mobile + ": Invalid mobile number!");
-            throw new CustomException("Invalid mobile number");
+            System.out.println("Mobile number is valid: " + validateMobileNumber.apply(mobile));
+            return true;
         }
-        return Pattern.matches(regex, mobile);
+        throw new CustomException("Invalid mobile number");
     }
     public static boolean preDefinedPasswordRule1() throws CustomException {
         String password = "qw9&ertyui";
-        String regex = "^.{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(password).matches())
+        Function<String, Boolean> validateMobileNumber = name -> name.matches("^.{8,}$");
+        if (validateMobileNumber.apply(password))
         {
-            System.out.println( password + ": Valid password!");
+            System.out.println("Password is valid: " + validateMobileNumber.apply(password));
+            return true;
         }
-        else
-        {
-            System.out.println(password + ": Invalid password.");
-            throw new CustomException("Invalid password");
-        }
-        return Pattern.matches(regex, password);
+        throw new CustomException("Invalid password");
     }
     public static boolean preDefinedPasswordRule2() throws CustomException {
         String password2 = "Kwertyui";
-        String regex = "^(?=.*[A-Z]).{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(password2).matches()) {
-            System.out.println( password2 + ": Valid password!");
-        }
-        else
+        Function<String, Boolean> validateMobileNumber = name -> name.matches("^(?=.*[A-Z]).{8,}$");
+        if (validateMobileNumber.apply(password2))
         {
-            System.out.println(password2 + ": Invalid password.");
-            throw new CustomException("Invalid password");
+            System.out.println("Password is valid: " + validateMobileNumber.apply(password2));
+            return true;
         }
-        return Pattern.matches(regex, password2);
+        throw new CustomException("Invalid password");
     }
     public static boolean preDefinedPasswordRule3() throws CustomException {
         String password3 = "K9wertyui";
-        //String[] inputs = {"K9wertyui", "vhn9KHhbh", "kjhgftv"};
-        String regex = "^(?=.*[A-Z])(?=.*[0-9]).{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(password3).matches()) {
-            System.out.println( password3 + ": Valid password!");
-        }
-        else
+        Function<String, Boolean> validateMobileNumber = name -> name.matches("^(?=.*[A-Z])(?=.*[0-9]).{8,}$");
+        if (validateMobileNumber.apply(password3))
         {
-            System.out.println(password3 + ": Invalid password.");
-            throw new CustomException("Invalid password");
+            System.out.println("Password is valid: " + validateMobileNumber.apply(password3));
+            return true;
         }
-        return Pattern.matches(regex, password3);
+        throw new CustomException("Invalid password");
     }
     public static boolean preDefinedPasswordRule4() throws CustomException {
         String password4 = "Ka7huyK79#A";
-        //String[] inputs = {"K9we&tyu#i", "Khn9Kh%bhp", "kjhgftv"};
-        String regex = "^(?=(?:[^!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]){1}[^!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$).*[^!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$";
-        //String regex = "^(?=[0-9]*[A-Z])(?=[a-zA-z]*[0-9]).{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(password4).matches()) {
-            System.out.println( password4 + ": Valid email!");
-        }
-        else
+        Function<String, Boolean> validateMobileNumber = name -> name.matches("^(?=(?:[^!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*[!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]){1}[^!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$).*[^!@#$%^&*()_+\\-=\\[\\]{};':\"\\\\|,.<>\\/?]*$");
+        if (validateMobileNumber.apply(password4))
         {
-            System.out.println(password4 + ": Invalid email.");
-            throw new CustomException("Invalid password");
+            System.out.println("Password is valid: " + validateMobileNumber.apply(password4));
+            return true;
         }
-        return Pattern.matches(regex, password4);
+        throw new CustomException("Invalid password");
     }
     public static boolean emailSamplesValidation() throws CustomException {
-        String[] inputs = {"abc.xyz@bl.co.in", "yhz.out&kl.co.in", "abc.ykj@bl.co.ik"};
-        String regex = "^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$";
-        Pattern pattern = Pattern.compile(regex);
-        for (String input: inputs) {
-            if (pattern.matcher(input).matches()) {
-                System.out.println( input + ": Valid email!");
+        String[] inputs = {"abc.xyz@bl.co.in", "yhz.out&kl.co.in", "abc.ykj@bl.co.ik","yhz.out&kl.co.in"};
+        //String regex = "^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$";
+        for(String input: inputs)
+        {
+            Function<String, Boolean> validateEmail = email -> email.matches("^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$");
+            if (validateEmail.apply(input))
+            {
+                System.out.println(input +" : is valid: " + validateEmail.apply(input));
+                //return true;
             }
             else
             {
-                System.out.println(input + ": Invalid email!");
-                throw new CustomException("Invalid email");
-
+                System.out.println(input +" : is valid: " + validateEmail.apply(input));
             }
         }
-        return true;
+        throw new CustomException("Invalid email");
+
     }
     public static boolean multipleEmailValidation(String email) throws CustomException {
-        //String[] inputs = {"abc.xyz@bl.co.in", "yhz.out&kl.co.in", "abc.ykj@bl.co.ik"};
         String regex = "^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$";
-        Pattern pattern = Pattern.compile(regex);
-        if (pattern.matcher(email).matches()) {
-            System.out.println( email + ": Valid email!");
-        }
-        else
+        Function<String, Boolean> validateMobileNumber = name -> name.matches("^abc\\.[A-Za-z]{1,}@bl\\.co\\.[A-Za-z]{2}$");
+        if (validateMobileNumber.apply(email))
         {
-            System.out.println(email + ": Invalid email!");
-             throw new CustomException("Invalid email");
+            System.out.println("Password is valid: " + validateMobileNumber.apply(email));
+            return true;
         }
-        return Pattern.matches(regex, email);
+        throw new CustomException("Invalid password");
     }
 }
